28659c2f68106afc33e4587ad4ca2718e23c39e1
Anna Lavrova <anna.laurier99@gmail.com>
2024-02-08 17:14:40 +0300

    Initial Commit

---
+++ what animal are you?/Models/Quastion.swift
@@ -0,0 +1,71 @@
+//
+//  Quastion.swift
+//  what animal are you?
+//
+//  Created by Anna Lavrova on 08.02.2024.
+//
+
+struct Question {
+    let title: String
+    let answers: [Answer]
+    let type: ResponseType
+
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пищу вы предпочитаете?",
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle)
+                ],
+
+                type: .multiple
+            ),
+
+            Question(
+                title: "Что вам нравится больше?",
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обниматься", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle)
+                ],
+
+                type: .ranged
+            )
+        ]
+    }
+}
+
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "😺"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+
+    var definition: String {
+        switch self {
+        case .dog:
+            return "Вам нравится мясо и гулять"
+        case .cat:
+            return "Вы любите лежать на солнце и есть рыбу"
+        case .rabbit:
+            return "Вы очень быстро принимаете решения"
+        case .turtle:
+            return "Вы довольно размеренно живете свою жизнь"
+        }
+    }
+}
---
+++ what animal are you?/View Controllers/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  what animal are you?
+//
+//  Created by Anna Lavrova on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
